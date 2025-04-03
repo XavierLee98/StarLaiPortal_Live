e30208fae70d65c06b21eb2831a16f84b0851611
Jun Chua <tjc@myfastrack.net>
2025-04-03 16:16:16 +0800

    20250304v1

--- PortalIntegration/Properties/Resources.Designer.cs
+++ PortalIntegration/Properties/Resources.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace PortalIntegration_Live_GSG.Properties {
+namespace PortalIntegration_Live.Properties {
     using System;
     
     
@@ -39,7 +39,7 @@ namespace PortalIntegration_Live_GSG.Properties {
         internal static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("PortalIntegration_Live_GSG.Properties.Resources", typeof(Resources).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("PortalIntegration_Live.Properties.Resources", typeof(Resources).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
